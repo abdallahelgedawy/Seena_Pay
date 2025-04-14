bc3fee06bce9fb24c4efe2b3052ba5eb74167ab2
Abdallah elgedawy <abdallahelgedawy1@gmail.com>
2025-04-14 20:56:29 +0200

    Initial Commit

---
+++ Seena_Pay/Seena_PayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seena_PayApp.swift
+//  Seena_Pay
+//
+//  Created by Abdallah Elgedawy on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Seena_PayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
